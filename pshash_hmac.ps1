38e6d75a6001ee7763cb3ee0d6830b82ca6a6cd3
tomblanchard312 <123529054+tomblanchard312@users.noreply.github.com>
2023-11-08 21:38:42 -0500

    pshash_hmac powershell script
    This basically performs the same as php hash_hmac but in PowerShell without passing algorithm it is hard-coded to HMACSHA512.
    So where php  hash_hmac need the following parameters:  string $algo,  string $data,  string $key,  bool $binary, this only needs the two parameters, $data and $key.

---
+++ pshash_hmac.ps1
@@ -0,0 +1,30 @@
+﻿#Returns Base16 Hashed Value
+#You can test here: https://dinochiesa.github.io/hmachash/index.html
+#Make sure that you have permissions to Add-Type or this will not work
+$id = get-random
+$key='secret'
+$data='The quick brown fox jumped over the lazy dog.'
+$Source = @"
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace pshash
+    {
+    public class hmac$id
+        {
+        public static string HMACSHA512(string key, string data)
+            {
+            string HmacHashed = "";
+            using (HMACSHA512 HMACSHA512 = new HMACSHA512(Encoding.ASCII.GetBytes(key)))
+                    {
+                      HmacHashed = BitConverter.ToString(HMACSHA512.ComputeHash(Encoding.ASCII.GetBytes(data))).Replace("-", string.Empty);
+                    }
+            return HmacHashed;
+            }
+        }
+    }
+"@
+Add-Type -TypeDefinition $Source -Language CSharp 
+$hashedvalue = Invoke-Expression "[pshash.hmac$id]::HMACSHA512('$key','$data')"
+Write-Host $hashedvalue
